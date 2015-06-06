8c10203b08635cb60c7089ee0d9d364ca88786d6
Rahul Nadella <rahulnadella@yahoo.com>
2015-06-06 15:50:48 -0400

    Updated the SunriseSunset class

--- src/com/sunrisesunset/SunriseSunset.java
+++ src/com/sunrisesunset/SunriseSunset.java
@@ -1,5 +1,10 @@
 package com.sunrisesunset;
 
+import static com.sunrisesunset.Zenith.ASTRONOMICAL;
+import static com.sunrisesunset.Zenith.CIVIL;
+import static com.sunrisesunset.Zenith.NAUTICAL;
+import static com.sunrisesunset.Zenith.OFFICIAL;
+
 import java.util.Calendar;
 import java.util.TimeZone;
 
@@ -20,67 +25,67 @@ public class SunriseSunset {
 	}
 	
 	public String getAstronomicalSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(Zenith.ASTRONOMICAL, date);
+		return solarEvent.computeSunriseTime(ASTRONOMICAL, date);
 	}
 	
 	public Calendar getAstronomicalSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(Zenith.ASTRONOMICAL, date);
+		return solarEvent.computeSunriseCalendar(ASTRONOMICAL, date);
 	}
 	
 	public String getAstronomicalSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(Zenith.ASTRONOMICAL, date);
+		return solarEvent.computeSunsetTime(ASTRONOMICAL, date);
 	}
 	
 	public Calendar getAstronomicalSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(Zenith.ASTRONOMICAL, date);
+		return solarEvent.computeSunsetCalendar(ASTRONOMICAL, date);
 	}
 	
 	public String getNauticalSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(Zenith.NAUTICAL, date);
+		return solarEvent.computeSunriseTime(NAUTICAL, date);
 	}
 	
 	public Calendar getNauticalSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(Zenith.NAUTICAL, date);
+		return solarEvent.computeSunriseCalendar(NAUTICAL, date);
 	}
 	
 	public String getNauticalSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(Zenith.NAUTICAL, date);
+		return solarEvent.computeSunsetTime(NAUTICAL, date);
 	}
 	
 	public Calendar getNauticalSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(Zenith.NAUTICAL, date);
+		return solarEvent.computeSunsetCalendar(NAUTICAL, date);
 	}
 	
 	public String getCivilSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(Zenith.CIVIL, date);
+		return solarEvent.computeSunriseTime(CIVIL, date);
 	}
 	
 	public Calendar getCivilSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(Zenith.CIVIL, date);
+		return solarEvent.computeSunriseCalendar(CIVIL, date);
 	}
 	
 	public String getCivilSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(Zenith.CIVIL, date);
+		return solarEvent.computeSunsetTime(CIVIL, date);
 	}
 	
 	public Calendar getCivilSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(Zenith.CIVIL, date);
+		return solarEvent.computeSunsetCalendar(CIVIL, date);
 	}
 	
 	public String getOfficialSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(Zenith.OFFICIAL, date);
+		return solarEvent.computeSunriseTime(OFFICIAL, date);
 	}
 	
 	public Calendar getOfficialSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(Zenith.OFFICIAL, date);
+		return solarEvent.computeSunriseCalendar(OFFICIAL, date);
 	}
 	
 	public String getOfficialSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(Zenith.OFFICIAL, date);
+		return solarEvent.computeSunsetTime(OFFICIAL, date);
 	}
 	
 	public Calendar getOfficialSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(Zenith.OFFICIAL, date);
+		return solarEvent.computeSunsetCalendar(OFFICIAL, date);
 	}
 	
 	public static Calendar getSunrise(double latitude, double longitude, TimeZone timeZone, Calendar date, double degrees) {
