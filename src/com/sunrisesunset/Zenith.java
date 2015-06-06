4d6e79be5142e54c26adc752757336611d5a6e1f
Rahul Nadella <rahulnadella@yahoo.com>
2015-06-06 11:36:03 -0400

    Initial commit of SunriseSunset project

---
+++ src/com/sunrisesunset/Zenith.java
@@ -0,0 +1,43 @@
+package com.sunrisesunset;
+
+import java.math.BigDecimal;
+
+/**
+ * The definition of Zenith is the point on the celestial sphere vertically
+ * above a given position or observer.
+ * 
+ * The {@link Zenith} class defines the solar declination used in computing the
+ * sunrise/sunset.
+ * 
+ * @version 1.0
+ */
+public class Zenith {
+	/**
+	 * Astronomical sunrise/set is when the sun is 18 degrees below the horizon.
+	 */
+	public static final Zenith ASTRONOMICAL = new Zenith(108);
+
+	/** Nautical sunrise/set is when the sun is 12 degrees below the horizon. */
+	public static final Zenith NAUTICAL = new Zenith(102);
+
+	/**
+	 * Civil sunrise/set (dawn/dusk) is when the sun is 6 degrees below the
+	 * horizon.
+	 */
+	public static final Zenith CIVIL = new Zenith(96);
+
+	/**
+	 * Official sunrise/set is when the sun is 50' below the horizon.
+	 */
+	public static final Zenith OFFICIAL = new Zenith(96);
+
+	protected final BigDecimal degrees;
+
+	public Zenith(double degrees) {
+		this.degrees = BigDecimal.valueOf(degrees);
+	}
+
+	public BigDecimal degrees() {
+		return degrees;
+	}
+}
