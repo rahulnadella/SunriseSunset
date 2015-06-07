50c9d8e73856e4fe45c22b9f3d33e42cbdaa09bb
Rahul Nadella <rahulnadella@yahoo.com>
2015-06-07 14:50:03 -0400

    Updated java docs

--- src/com/sunrisesunset/api/SunriseSunset.java
+++ src/com/sunrisesunset/api/SunriseSunset.java
@@ -11,118 +11,238 @@ import java.util.TimeZone;
 
 import com.sunrisesunset.util.SunriseSunsetUtility;
 
+/**
+ * Sunrise or sun up is the instant at which the upper edge of the Sun appears
+ * over the eastern horizon in the morning. The term can also refer to the
+ * entire process of the Sun crossing the horizon and its accompanying
+ * atmospheric effects.
+ * 
+ * Sunset is the time of sunset is defined in astronomy as the moment when the
+ * trailing edge of the Sun's disk disappears below the horizon. The ray path of
+ * light from the setting Sun is highly distorted near the horizon because of
+ * atmospheric refraction, making the sunset appear to occur when the Sun’s disk
+ * is already about one diameter below the horizon. Sunset is distinct from
+ * dusk, which is the time at which the sky becomes completely dark, which
+ * occurs when the Sun is approximately eighteen degrees below the horizon. The
+ * period between sunset and dusk is called twilight.
+ * 
+ * The {@link SunriseSunset} class provides the ability to retrieve the
+ * sunrise/sunset and 3 types of {@code Twilight} based on specific timezone,
+ * latitude and longitude.
+ * 
+ * @version 1.0
+ */
 public class SunriseSunset {
-
+	/* The values associated to the specific Location */
 	protected BigDecimal latitude;
 	protected BigDecimal longitude;
+	/* The value to set to calculate the sunrise/sunset */
+	protected SunriseSunsetUtility sunrisesunset;
 
-	protected SunriseSunsetUtility solarEvent;
-
+	/**
+	 * The {@link SunriseSunset} initialization based on time zone
+	 * {@code String}, latitude {@code BigDecimal}, and longitude
+	 * {@code BigDecimal}
+	 * 
+	 * @param timeZoneIdentifier
+	 *            A {@code String} based on the time zone is a region that
+	 *            observes a uniform standard time for legal, commercial, and
+	 *            social purposes.
+	 * @param latitude
+	 *            A {@code BigDecimal} value that represents latitude of the
+	 *            specific location
+	 * @param longitude
+	 *            A {@code BigDecimal} value that represents longitude of the
+	 *            specific location
+	 */
 	public SunriseSunset(String timeZoneIdentifier, BigDecimal latitude,
 			BigDecimal longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
-		this.solarEvent = new SunriseSunsetUtility(timeZoneIdentifier,
+		this.sunrisesunset = new SunriseSunsetUtility(timeZoneIdentifier,
 				latitude, longitude);
 	}
 
+	/**
+	 * The {@link SunriseSunset} initialization based on time zone
+	 * {@code TimeZone}, latitude {@code BigDecimal}, and longitude
+	 * {@code BigDecimal}
+	 * 
+	 * @param timeZoneIdentifier
+	 *            A {@code TimeZone} based on the time zone is a region that
+	 *            observes a uniform standard time for legal, commercial, and
+	 *            social purposes.
+	 * @param latitude
+	 *            A {@code BigDecimal} value that represents latitude of the
+	 *            specific location
+	 * @param longitude
+	 *            A {@code BigDecimal} value that represents longitude of the
+	 *            specific location
+	 */
 	public SunriseSunset(TimeZone timeZone, BigDecimal latitude,
 			BigDecimal longitude) {
 		this.latitude = latitude;
 		this.longitude = longitude;
-		this.solarEvent = new SunriseSunsetUtility(timeZone, latitude,
+		this.sunrisesunset = new SunriseSunsetUtility(timeZone, latitude,
 				longitude);
 	}
 
+	/**
+	 * The {@link SunriseSunset} initialization based on time zone
+	 * {@code String}, latitude {@code Double}, and longitude
+	 * {@code Double}
+	 * 
+	 * @param timeZoneIdentifier
+	 *            A {@code String} based on the time zone is a region that
+	 *            observes a uniform standard time for legal, commercial, and
+	 *            social purposes.
+	 * @param latitude
+	 *            A {@code Double} value that represents latitude of the
+	 *            specific location
+	 * @param longitude
+	 *            A {@code Double} value that represents longitude of the
+	 *            specific location
+	 */
 	public SunriseSunset(String timeZoneIdentifier, Double latitude,
 			Double longitude) {
 		this.latitude = new BigDecimal(latitude);
 		this.longitude = new BigDecimal(longitude);
-		this.solarEvent = new SunriseSunsetUtility(timeZoneIdentifier,
+		this.sunrisesunset = new SunriseSunsetUtility(timeZoneIdentifier,
 				latitude, longitude);
 	}
 
+	/**
+	 * The {@link SunriseSunset} initialization based on time zone
+	 * {@code TimeZone}, latitude {@code Double}, and longitude
+	 * {@code Double}
+	 * 
+	 * @param timeZoneIdentifier
+	 *            A {@code TimeZone} based on the time zone is a region that
+	 *            observes a uniform standard time for legal, commercial, and
+	 *            social purposes.
+	 * @param latitude
+	 *            A {@code Double} value that represents latitude of the
+	 *            specific location
+	 * @param longitude
+	 *            A {@code Double} value that represents longitude of the
+	 *            specific location
+	 */
 	public SunriseSunset(TimeZone timeZone, Double latitude, Double longitude) {
 		this.latitude = new BigDecimal(latitude);
 		this.longitude = new BigDecimal(longitude);
-		this.solarEvent = new SunriseSunsetUtility(timeZone, latitude,
+		this.sunrisesunset = new SunriseSunsetUtility(timeZone, latitude,
 				longitude);
 	}
 
-	public SunriseSunset(String timeZoneIdentifier, String latitude, String longitude) {
+	/**
+	 * The {@link SunriseSunset} initialization based on time zone
+	 * {@code String}, latitude {@code String}, and longitude
+	 * {@code String}
+	 * 
+	 * @param timeZoneIdentifier
+	 *            A {@code String} based on the time zone is a region that
+	 *            observes a uniform standard time for legal, commercial, and
+	 *            social purposes.
+	 * @param latitude
+	 *            A {@code String} value that represents latitude of the
+	 *            specific location
+	 * @param longitude
+	 *            A {@code String} value that represents longitude of the
+	 *            specific location
+	 */
+	public SunriseSunset(String timeZoneIdentifier, String latitude,
+			String longitude) {
 		this.latitude = new BigDecimal(latitude);
 		this.longitude = new BigDecimal(longitude);
-		this.solarEvent = new SunriseSunsetUtility(timeZoneIdentifier, latitude, longitude);
+		this.sunrisesunset = new SunriseSunsetUtility(timeZoneIdentifier,
+				latitude, longitude);
 	}
-	
+
+	/**
+	 * The {@link SunriseSunset} initialization based on time zone
+	 * {@code TimeZone}, latitude {@code String}, and longitude
+	 * {@code String}
+	 * 
+	 * @param timeZoneIdentifier
+	 *            A {@code TimeZone} based on the time zone is a region that
+	 *            observes a uniform standard time for legal, commercial, and
+	 *            social purposes.
+	 * @param latitude
+	 *            A {@code String} value that represents latitude of the
+	 *            specific location
+	 * @param longitude
+	 *            A {@code String} value that represents longitude of the
+	 *            specific location
+	 */
 	public SunriseSunset(TimeZone timeZone, String latitude, String longitude) {
 		this.latitude = new BigDecimal(latitude);
 		this.longitude = new BigDecimal(longitude);
-		this.solarEvent = new SunriseSunsetUtility(timeZone, latitude, longitude);
+		this.sunrisesunset = new SunriseSunsetUtility(timeZone, latitude,
+				longitude);
 	}
-	
+
 	public String getAstronomicalSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(ASTRONOMICAL, date);
+		return sunrisesunset.computeSunriseTime(ASTRONOMICAL, date);
 	}
 
 	public Calendar getAstronomicalSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(ASTRONOMICAL, date);
+		return sunrisesunset.computeSunriseCalendar(ASTRONOMICAL, date);
 	}
 
 	public String getAstronomicalSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(ASTRONOMICAL, date);
+		return sunrisesunset.computeSunsetTime(ASTRONOMICAL, date);
 	}
 
 	public Calendar getAstronomicalSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(ASTRONOMICAL, date);
+		return sunrisesunset.computeSunsetCalendar(ASTRONOMICAL, date);
 	}
 
 	public String getNauticalSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(NAUTICAL, date);
+		return sunrisesunset.computeSunriseTime(NAUTICAL, date);
 	}
 
 	public Calendar getNauticalSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(NAUTICAL, date);
+		return sunrisesunset.computeSunriseCalendar(NAUTICAL, date);
 	}
 
 	public String getNauticalSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(NAUTICAL, date);
+		return sunrisesunset.computeSunsetTime(NAUTICAL, date);
 	}
 
 	public Calendar getNauticalSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(NAUTICAL, date);
+		return sunrisesunset.computeSunsetCalendar(NAUTICAL, date);
 	}
 
 	public String getCivilSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(CIVIL, date);
+		return sunrisesunset.computeSunriseTime(CIVIL, date);
 	}
 
 	public Calendar getCivilSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(CIVIL, date);
+		return sunrisesunset.computeSunriseCalendar(CIVIL, date);
 	}
 
 	public String getCivilSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(CIVIL, date);
+		return sunrisesunset.computeSunsetTime(CIVIL, date);
 	}
 
 	public Calendar getCivilSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(CIVIL, date);
+		return sunrisesunset.computeSunsetCalendar(CIVIL, date);
 	}
 
 	public String getOfficialSunriseForDate(Calendar date) {
-		return solarEvent.computeSunriseTime(OFFICIAL, date);
+		return sunrisesunset.computeSunriseTime(OFFICIAL, date);
 	}
 
 	public Calendar getOfficialSunriseCalendarForDate(Calendar date) {
-		return solarEvent.computeSunriseCalendar(OFFICIAL, date);
+		return sunrisesunset.computeSunriseCalendar(OFFICIAL, date);
 	}
 
 	public String getOfficialSunsetForDate(Calendar date) {
-		return solarEvent.computeSunsetTime(OFFICIAL, date);
+		return sunrisesunset.computeSunsetTime(OFFICIAL, date);
 	}
 
 	public Calendar getOfficialSunsetCalendarForDate(Calendar date) {
-		return solarEvent.computeSunsetCalendar(OFFICIAL, date);
+		return sunrisesunset.computeSunsetCalendar(OFFICIAL, date);
 	}
 
 	public static Calendar getSunrise(double latitude, double longitude,
